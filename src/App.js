ec9662dcc1a1dc8a568b3ff41831a1738bd962ae
Ewelina Nieweglowska <nieweglowska.ewelina@gmail.com>
2022-08-23 21:08:17 +0200

    first step to implement a navbar with React Router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 // import Button from "./Button/Button";
 import Navbar from './Navbar/Navbar'
-import Article from './Article/Article'
+// import Article from './Article/Article';
+import Destination from './Destination/Destination'
+import Crew from './Crew/Crew'
 
 function App() {
+  let component
+  switch (window.location.pathname) {
+    case "/":
+      component = <App />
+      break
+    case "/destination":
+      component = <Destination />
+      break
+    case "/crew":
+      component = <Crew />
+      break
+    // case "/technology":
+    //   Component = Technology
+    //   break
+  }
   return (
-    <main className="container_home">
+    <>
       <Navbar />
-      <Article />
-    </main>
+      { component}
+    </>
   );
 }
 
