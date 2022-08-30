4e68e58f1f21c2c7976e60d9fa7e543c3fdbb850
Ewelina Nieweglowska <nieweglowska.ewelina@gmail.com>
2022-08-30 14:51:01 +0200

    CustomLink moved to global file

---
+++ src/utils.js
@@ -0,0 +1,20 @@
+import { Link, useMatch, useResolvedPath } from "react-router-dom";
+
+export default function CustomLink({ to, children, ...props }) {
+    // console.log("to", to);
+    // console.log("children", to, children);
+    // console.log("props", to, props);
+    const resolvedPath = useResolvedPath(to);
+    const isActive = useMatch({ path: resolvedPath.pathname, end: true });
+
+    // console.log("resolvedPath", to, resolvedPath);
+    console.log("isActive", to, isActive);
+
+    return (
+        <li className={isActive ? "active" : ""}>
+            <Link to={to} {...props}>{children}</Link>
+        </li>
+    )
+}
+
+
