a05ed3e499671aaa91afc78ec654d0a97089cc58
Ewelina Nieweglowska <nieweglowska.ewelina@gmail.com>
2022-09-30 12:41:30 +0200

    added media queries to home

--- src/features/SpaceTourismWebsite/home/article/styled.js
+++ src/features/SpaceTourismWebsite/home/article/styled.js
@@ -8,6 +8,15 @@ export const HomeArticle = styled.article`
     align-items: flex-end;
     height: 720px;
     padding: 130px 0 50px 0;
+    
+    @media (max-width: 767px) {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: start; 
+        padding: 0;
+        margin: 0;
+    }
 `;
 
 export const TextSection = styled.section`
@@ -15,6 +24,11 @@ export const TextSection = styled.section`
     padding-right: 70px;
     padding-bottom: 50px;
     padding-left: 100px;
+
+    @media (max-width: 767px){
+        width: 327px;
+        padding: 0;
+    }
 `;
 
 export const SmallHeader = styled.p`
@@ -23,6 +37,13 @@ export const SmallHeader = styled.p`
     font-size: 28px;
     line-height: 34px;
     letter-spacing: 4.725px;
+
+     @media (max-width: 767px){
+        font-size: 16px;
+        line-height: 19px;
+        text-align: center;
+        letter-spacing: 2.7px;
+    }
 `;
 
 export const BigHeader = styled.p`
@@ -32,6 +53,13 @@ export const BigHeader = styled.p`
     font-size: 150px;
     line-height: 172px;
     margin: 0;
+    color: #FFFFFF;
+
+    @media (max-width: 767px){
+        font-size: 80px;
+        line-height: 100px;
+        text-align: center;
+    }
 `;
 
 export const Content = styled.p`
@@ -39,10 +67,16 @@ export const Content = styled.p`
     font-family: 'Barlow';
     font-size: 18px;
     line-height: 32px;
+
+    @media (max-width: 767px){
+        font-size: 15px;
+        line-height: 25px;
+        text-align: center;
+    }
 `;
 
 export const HomeButton = styled.button`
-    display:block;
+    display: block;
     border-radius: 50%;
     border: 88px transparent;
     letter-spacing: 2px;
@@ -55,6 +89,15 @@ export const HomeButton = styled.button`
         background: rgba(255, 255, 255, 0.04);
         backdrop-filter: blur(81.5485px);
     }
+
+    @media (max-width: 767px) {
+        margin: 0;
+
+        &:hover{
+        background: transparent;
+    }
+    }
+
 `;
 
 export const BtnSpan = styled.span`
@@ -69,4 +112,11 @@ export const BtnSpan = styled.span`
     font-family: 'Bellefair';
     font-size: 32px;
     text-transform: uppercase;
+
+    @media (max-width: 767px) {
+        margin: 0;
+        font-size: 20px;
+        width: 150px;
+        height: 150px;
+    }
 `;
--- src/features/SpaceTourismWebsite/home/styled.js
+++ src/features/SpaceTourismWebsite/home/styled.js
@@ -1,9 +1,18 @@
 import styled from "styled-components";
-import imgHome from "./images/background-home-desktop.jpg";
+import imgHomeDesktop from "./images/background-home-desktop.jpg";
+import imgHomeMobile from "./images/background-home-mobile.jpg";
 
 export const Container = styled.main`
-    background-image: url(${imgHome});
+    background-image: url(${imgHomeDesktop});
     background-position: bottom;
     background-repeat: no-repeat;
     background-size: cover;
-`
+
+    @media (max-width: 768px) {
+        background-image: url(${imgHomeMobile});
+        background-position: bottom;
+        background-repeat: no-repeat;
+        background-size: cover;
+        max-height: 667px;
+    }
+`;
