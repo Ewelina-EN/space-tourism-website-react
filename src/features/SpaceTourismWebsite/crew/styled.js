56a529a4176ea27a7572df146b721ff8d285f8a2
Ewelina Nieweglowska <nieweglowska.ewelina@gmail.com>
2022-10-08 19:06:42 +0200

    added style to crew for mobile devices

--- src/features/SpaceTourismWebsite/crew/styled.js
+++ src/features/SpaceTourismWebsite/crew/styled.js
@@ -1,12 +1,17 @@
 import styled from "styled-components";
 import imgCrewDesktop from "./images/background-crew-desktop.jpg";
+import imgCrewTablet from "./images/background-crew-tablet.jpg";
 import imgCrewMobile from "./images/background-crew-mobile.jpg";
 
 export const ContainerCrew = styled.main`
     background-image: url(${imgCrewDesktop});
     background-position: bottom;
     background-repeat: no-repeat;
-    background-size: cover;
+    background-size: cover; 
+
+    @media (max-width: 768px) {
+        background-image: url(${imgCrewTablet});
+    }
 
     @media (max-width: 375px) {
         background-image: url(${imgCrewMobile});
@@ -22,6 +27,13 @@ export const Header = styled.div`
     font-size: 28px;
     letter-spacing: 4.725px;
     padding: 70px 0 0 150px;
+
+    @media (max-width: 768px) {
+        padding: 40px 0 0 40px;
+        font-size: 20px;
+        line-height: 24px;
+        letter-spacing: 3.375px;
+    }
 
     @media (max-width: 375px) {
         font-size: 16px;
@@ -46,6 +58,13 @@ export const Content = styled.article`
     padding: 70px 0 0;
     margin: 0;
 
+    @media (max-width: 768px) {
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        padding: 0;
+    }
+
     @media (max-width: 375px) {
         display:none;
     }
@@ -53,6 +72,14 @@ export const Content = styled.article`
 
 export const SectionCrew = styled.div`
     margin: 50px 80px 0 0;
+
+    @media (max-width: 768px) {
+        margin: 60px 0 0 0;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+    }
 
     @media (max-width: 375px) {
         margin: 0;
@@ -70,6 +97,13 @@ export const Role = styled.p`
     color: #FFFFFF;
     opacity: 0.5;
 
+    @media (max-width: 768px) {
+        font-size: 24px;
+        line-height: 28px;
+        text-align: center;
+        margin: 15px 0 0 0;
+    }
+
     @media (max-width: 375px) {
         font-size: 16px;
         line-height: 18px;
@@ -86,6 +120,13 @@ export const Name = styled.p`
     color: #FFFFFF;
     margin: 13px 0 0 0;
 
+    @media (max-width: 768px) {
+        font-size: 40px;
+        line-height: 46px;
+        text-align: center;
+        margin: 8px 0 0 0;
+    }
+
     @media (max-width: 375px) {
         font-size: 24px;
         line-height: 28px;
@@ -101,6 +142,13 @@ export const Info = styled.p`
     width: 444px;
     margin: 29px 0 0 0;
 
+    @media (max-width: 768px) {
+        font-size: 16px;
+        line-height: 28px;
+        text-align: center;
+        margin: 16px 0 0 0;
+    }
+
     @media (max-width: 375px) {
         font-size: 15px;
         line-height: 25px;
@@ -113,6 +161,13 @@ export const Info = styled.p`
 export const Img = styled.img`
     display: inline-block;
     height: 600px;
+
+    @media (max-width: 768px) {
+        height: 572px;
+        margin-top: 32px;
+        border-bottom: 2px solid #383B4B;
+        padding: 0 80px;
+    }
     
     @media (max-width: 375px) {
         height: 223px;
@@ -129,6 +184,11 @@ export const CrewNavigation = styled.nav`
     display: flex;
     margin-top: 70px;
     padding: 0;
+    
+    @media (max-width: 768px) {
+        align-items: center;
+        margin-top: 40px;
+    }
 
     @media (max-width: 375px) {
         display:flex;
@@ -146,6 +206,13 @@ export const MenuList = styled.ul`
     padding: 0;
     list-style: none;
 
+    @media (max-width: 768px) {
+        font-size: 16px;
+        line-height: 18px;
+        text-align: center;
+        margin: 0;
+    }
+
     @media (max-width: 375px) {
         display: flex;
         align-items:center;
@@ -160,6 +227,13 @@ export const Span = styled.span`
     width: 20px;
     height: 20px;
     background-color: #FFFFFF;
+
+    @media (max-width: 768px) {
+        width: 10px;
+        height: 10px;
+        margin-right: 16px;
+        background-color: #FFFFFF;
+    }
 
     @media (max-width: 375px) {
         margin: 0 10px;
